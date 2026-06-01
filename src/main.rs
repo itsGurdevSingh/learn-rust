68bf709db9c3155ae58561d786c2a8e0bf2711f1
gurdevsinghvip@gmail.com <gurdevsinghvip@gmail.com>
2026-06-01 12:00:36 +0530

    add string length function call in main

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 fn main() {
     println!("{}", is_even(20));
 
-    println!("fib value is {}", fib(5))
+    println!("fib value is {}", fib(5));
+
+
+    // call get string length function 
+    let name = String::from("gurdev");
+    let len = get_str_len(name);
+    println!("length of name string is {}", len)
 }
 
 // find even number
@@ -34,4 +40,10 @@ fn fib(num: u32) -> u32 {
     }
 
     return second;
+}
+
+
+// get length of string
+fn get_str_len(s: String) -> usize {
+    return s.chars().count();
 }
