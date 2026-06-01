69b9fc147a4639e9556f81d061b5137b21792329
gurdevsinghvip@gmail.com <gurdevsinghvip@gmail.com>
2026-06-01 12:39:44 +0530

    Add Rect struct with area, perimeter, and debug methods; update main to demonstrate functionality

--- struct-in-rust/src/main.rs
+++ struct-in-rust/src/main.rs
@@ -5,6 +5,27 @@ struct User {
 }
 
 
+struct Rect {
+    width: u32,
+    height: u32,
+}
+
+impl Rect{
+
+    fn area(&self) -> u32{
+        self.width * self.height
+    }
+
+    fn parimeter(&self) -> u32 {
+        2*(self.width + self.height)
+    }
+
+    // like a static function in js classes 
+    fn debug() -> u32 {
+        return 1
+    }
+}
+
 fn main() {
     
     let user = User {
@@ -15,4 +36,17 @@ fn main() {
 
     println!("name of user is {}", user.first_name);
     
+    
+    let rect1 = Rect{
+        width: 20,
+        height: 10,
+    };
+
+    println!("area of rect1 is {}" , rect1.area());
+    println!("parimeter of rect1 is {}", rect1.parimeter());
+
+
+    // static function or fuction withour self refrence are called on struct itself on in its objects 
+    println!("run debug fn on Rect return value is {}", Rect::debug())
+ 
 }
