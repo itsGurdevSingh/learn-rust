d036fa464e09464a5d2c71668c199865c16bb11b
gurdevsinghvip@gmail.com <gurdevsinghvip@gmail.com>
2026-06-03 17:16:16 +0530

    Refactor calculate function to accept references and improve readability in main

--- generics-in-rust/src/main.rs
+++ generics-in-rust/src/main.rs
@@ -3,13 +3,15 @@ use std::ops::{ Add, Div, Mul, Sub};
 fn main() {
     println!("largest num is {}", find_largest(2,3));
 
-    let result = calculate(2, "+", 3);
+    let num1 = 2;
+    let num2 = 3;
+    let operator = "+";
+
+    let result = calculate(&num1, "+", &num2);
     match result {
-        Some(sum) => println!("addition of the 2 and 3 {}", sum),
+        Some(sum) => println!("Result of {} {} {} = {}", num1,operator,num2 , sum),
         None => {}
     }
-
-    
 }
 
 fn find_largest<T: std::cmp::PartialOrd>(a: T, b: T) -> T {
@@ -19,14 +21,14 @@ fn find_largest<T: std::cmp::PartialOrd>(a: T, b: T) -> T {
     return b;
 }
 
-fn calculate<T: Add + Sub + Mul + Div>(num1:T, operator: &str, num2: T) -> Option<T>
+fn calculate<T: Add + Sub + Mul + Div + Copy>(num1:&T, operator: &str, num2: &T) -> Option<T>
  where T: Add<Output = T> + Sub<Output = T> + Mul<Output = T> + Div<Output = T>
  {
     match operator {
-        "+" => Some(num1 + num2),
-        "-" => Some(num1 - num2),
-        "*" => Some(num1 * num2),
-        "/" => Some(num1 / num2),
+        "+" => Some(*num1 + *num2),
+        "-" => Some(*num1 - *num2),
+        "*" => Some(*num1 * *num2),
+        "/" => Some(*num1 / *num2),
         _ => None
     }
 }
